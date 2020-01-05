6abd27ecaf0a5f19256b47de5bd4860a87f1ab39
HarjaapSinghM <harjaap.makkar@gmail.com>
2020-01-05 15:08:15 -0500

    Favicon and projects added

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,15 @@ const imagemin = require("gulp-imagemin");
 
 function imgSquash() {
     return gulp 
-        .src("tourourhomes/projects/oakville-modern-luxury/images/*")
+        .src("tourourhomes/projects/richmond-hill-radiance/images/*")
         .pipe(imagemin({ progressive: true, optimizationLevel: 7 }))
-        .pipe(gulp.dest("tourourhomes/projects/oakville-modern-luxury/minified"));
+        .pipe(gulp.dest("tourourhomes/projects/richmond-hill-radiance/minified"));
 }
 
 gulp.task("imgSquash", imgSquash);
 
 gulp.task("watch", () => {
-    gulp.watch("tourourhomes/projects/oakville-modern-luxury/images/*", imgSquash);
+    gulp.watch("tourourhomes/projects/richmond-hill-radiance/images/*", imgSquash);
 });
 
 gulp.task("default", gulp.series("imgSquash", "watch"));
