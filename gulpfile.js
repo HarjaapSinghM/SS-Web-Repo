d962aea1c8d5701ddbd1d0df618264a4f6748ccb
HarjaapSinghM <harjaap.makkar@gmail.com>
2020-01-05 13:15:39 -0500

    Added More Project Pages

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,15 @@ const imagemin = require("gulp-imagemin");
 
 function imgSquash() {
     return gulp 
-        .src("tourourhomes/projects/liberty-village-lifestyle/images/*")
+        .src("tourourhomes/projects/oakville-modern-luxury/images/*")
         .pipe(imagemin({ progressive: true, optimizationLevel: 7 }))
-        .pipe(gulp.dest("tourourhomes/projects/liberty-village-lifestyle/minified"));
+        .pipe(gulp.dest("tourourhomes/projects/oakville-modern-luxury/minified"));
 }
 
 gulp.task("imgSquash", imgSquash);
 
 gulp.task("watch", () => {
-    gulp.watch("tourourhomes/beforeafter/images/*", imgSquash);
+    gulp.watch("tourourhomes/projects/oakville-modern-luxury/images/*", imgSquash);
 });
 
 gulp.task("default", gulp.series("imgSquash", "watch"));
