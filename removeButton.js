61f89c3e560d39625afe05ed9f6e469a1d0e7d61
HarjaapSinghM <harjaap.makkar@gmail.com>
2020-01-12 09:09:24 -0500

    Update removeButton.js

--- removeButton.js
+++ removeButton.js
@@ -3,7 +3,7 @@ $(function() {
 
 
     function sample() {
-        var linksToGoogle = $('a[href="https://elfsight.com/google-reviews-widget/?utm_source=websites&utm_medium=clients&utm_content=google-reviews&utm_term=undefined&utm_campaign=free-widget"]');
+        var linksToGoogle = $('a[href="https://elfsight.com/google-reviews-widget/?utm_source=websites&utm_medium=clients&utm_content=google-reviews&utm_term=harjaapsinghm.github.io&utm_campaign=free-widget"]');
         console.log(linksToGoogle);
         linksToGoogle.css("display","none");
      };
